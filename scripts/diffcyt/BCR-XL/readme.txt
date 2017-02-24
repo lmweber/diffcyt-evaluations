a5c6ba84fcc448dbb10f53d3864c95da89ebec7e
Lukas Weber <lukmweber@gmail.com>
2017-02-24 15:42:10 +0100

    script to run complete pipeline

---
+++ scripts/diffcyt/BCR-XL/readme.txt
@@ -0,0 +1,1 @@
+Run script "run_pipeline_BCRXL.R" to run the complete pipeline, i.e. all scripts in the correct order.
---
+++ scripts/diffcyt/BCR-XL/run_pipeline_BCRXL.R
@@ -0,0 +1,10 @@
+#########################################################
+# Script to run complete pipeline for data set 'BCR-XL' #
+#########################################################
+
+
+source("load_data_BCRXL.R")
+source("transform_BCRXL.R")
+source("clustering_BCRXL.R")
+
+
