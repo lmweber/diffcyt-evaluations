7846cfad6cfa14895fb4b5decd262e4495f78ed6
Lukas Weber <lukmweber@gmail.com>
2017-05-17 14:20:17 +0200

    script to prepare data set BCR_XL_all

---
+++ prepare_data/BCR_XL_all/prepare_data_BCR_XL_all.R
@@ -0,0 +1,34 @@
+##########################################################################################
+# Script to prepare benchmark data set 'BCR-XL-all'
+#
+# 'BCR-XL' data set is sourced from Bodenmiller et al. (2012), and was previously used 
+# for benchmark evaluations by Bruggner et al. (2014) (Citrus paper)
+#
+# Raw data downloaded from Cytobank (experiment 15713)
+# - see Citrus wiki (section "PBMC Example 1"):
+# https://github.com/nolanlab/citrus/wiki/PBMC-Example-1
+# - direct link to Cytobank repository:
+# https://community.cytobank.org/cytobank/experiments/15713/download_files
+#
+# Lukas Weber, May 2017
+##########################################################################################
+
+
+# Select files from BCR-XL and Reference conditions, and copy to 'data' folder. No 
+# additional pre-processing required.
+
+DIR_RAW_DATA <- "../../../../benchmark_data/BCR_XL_all/raw_data/experiment_15713_files"
+DIR_DATA <- "../../../../benchmark_data/BCR_XL_all/data"
+
+files <- list.files(DIR_RAW_DATA, pattern = "\\.fcs$", full.names = TRUE)
+
+files_BCRXL <- files[grep("patient[1-8]_BCR-XL\\.fcs$", files)]
+files_ref <- files[grep("patient[1-8]_Reference\\.fcs$", files)]
+
+cmds <- paste("cp", c(files_BCRXL, files_ref), DIR_DATA)
+
+for (cmd in cmds) {
+  system(cmd)
+}
+
+
