de9aa130eb82716e0c473c8b3f1a48aef690bad7
Lukas Weber <lukmweber@gmail.com>
2017-05-23 15:37:13 +0200

    rewrite analysis script for 'BCR-XL-all' data set

--- evaluate_methods/diffcyt/diffcyt_analysis_BCR_XL_all.R
+++ evaluate_methods/diffcyt/diffcyt_analysis_BCR_XL_all.R
@@ -1,30 +1,28 @@
-###################################################################
-# Script to run 'diffcyt' analysis pipeline for data set 'BCR-XL' #
-# Lukas Weber, March 2017                                         #
-###################################################################
+##########################################################################################
+# Script to run 'diffcyt' methods for data set 'BCR-XL-all'
+#
+# Lukas Weber, May 2017
+##########################################################################################
 
 
 library(diffcyt)
 library(flowCore)
 library(SummarizedExperiment)
-library(limma)
 
 
 
-####################################
-# (1) Load data and pre-processing #
-####################################
+##############################
+# Load data and pre-processing
+##############################
 
 # ---------
 # load data
 # ---------
 
 # filenames
-files <- list.files("../../../../benchmark_data/Citrus_paper_data/experiment_15713_files", 
-                    pattern = "\\.fcs$", full.names = TRUE)
-
-files_BCRXL <- files[grep("patient[1-8]_BCR-XL\\.fcs$", files)]
-files_ref <- files[grep("patient[1-8]_Reference\\.fcs$", files)]
+files <- list.files("../../../../benchmark_data/BCR_XL/data", pattern = "\\.fcs$", full.names = TRUE)
+files_BCRXL <- files[grep("BCR-XL", files)]
+files_ref <- files[grep("Reference", files)]
 
 # load data
 files_load <- c(files_BCRXL, files_ref)
@@ -35,7 +33,6 @@ d_input <- lapply(files_load, read.FCS, transformation = FALSE, truncate_max_ran
 # sample IDs and group IDs
 sample_IDs <- gsub("^PBMC8_30min_", "", gsub("\\.fcs$", "", basename(files_load)))
 sample_IDs
-
 group_IDs <- gsub("^patient[0-9]_", "", sample_IDs)
 group_IDs
 
@@ -43,27 +40,27 @@ group_IDs
 group_IDs <- factor(group_IDs, levels = c("Reference", "BCR-XL"))
 group_IDs
 
-# indices of all marker columns, lineage markers, and functional markers (see Table 1 in
-# Bruggner et al. 2014)
-marker_cols <- c(3:4, 7:9, 11:19, 21:22, 24:26, 28:31, 33)
-lineage_cols <- c(3:4, 9, 11,12,14, 21, 29, 31, 33)
-functional_cols <- setdiff(marker_cols, lineage_cols)
+# indices of all marker columns, lineage markers, and functional markers
+# (see Table 1 in Bruggner et al. 2014)
+cols_markers <- c(3:4, 7:9, 11:19, 21:22, 24:26, 28:31, 33)
+cols_lineage <- c(3:4, 9, 11, 12, 14, 21, 29, 31, 33)
+cols_func <- setdiff(cols_markers, cols_lineage)
 
 # prepare data into required format
 # (note: using lineage markers for clustering, and functional markers for DE testing)
-d_se <- prepareData(d_input, sample_IDs, marker_cols, lineage_cols, functional_cols)
+d_se <- prepareData(d_input, sample_IDs, cols_markers, cols_lineage, cols_func)
 
 # check markers (see Table 1, Bruggner et al. 2014)
-colnames(d_se)[lineage_cols]
-colnames(d_se)[functional_cols]
+colnames(d_se)[cols_lineage]
+colnames(d_se)[cols_func]
 
 
-# ---------
-# transform
-# ---------
+# --------------
+# transform data
+# --------------
 
-# transform all marker columns (using asinh with cofactor = 5; see Bendall et al. 2011, 
-# Supp. Fig. S2)
+# transform all marker columns
+# (using asinh with cofactor = 5; see Bendall et al. 2011, Supp. Fig. S2)
 d_se <- transformData(d_se, cofactor = 5)
 
 
@@ -71,23 +68,25 @@ d_se <- transformData(d_se, cofactor = 5)
 # clustering
 # ----------
 
-# generate micro-clusters
-d_se <- generateClusters(d_se, xdim = 20, ydim = 20, seed = 123)
+# generate mini-clusters
+seed <- 123
+d_se <- generateClusters(d_se, xdim = 20, ydim = 20, seed = seed)
 
 # check
-length(rowData(d_se)$cluster)         # number of cells
+nrow(rowData(d_se))                   # number of cells
+sum(table(rowData(d_se)$cluster))
 length(table(rowData(d_se)$cluster))  # number of clusters
 
 
-# --------------------------------------------
-# calculate cluster counts, medians, and ECDFs
-# --------------------------------------------
+# ---------------------------------------------
+# calculate cluster cell counts, medians, ECDFs
+# ---------------------------------------------
 
 # calculate cluster cell counts
 d_counts <- calcCounts(d_se)
 dim(d_counts)
 
-# calculate cluster medians (median marker expression)
+# calculate cluster medians
 d_medians <- calcMedians(d_se)
 dim(d_medians)
 
@@ -96,92 +95,168 @@ d_ecdfs <- calcECDFs(d_se)
 dim(d_ecdfs)
 
 
+# --------------------------
+# block IDs for paired tests
+# --------------------------
 
-######################################################
-# (2) Test for differentially abundant (DA) clusters #
-######################################################
-
-# check group IDs factor has correct level as reference
-group_IDs
-
-# check order of group IDs factor matches with order of samples (columns) in data objects
-colnames(d_counts)
-colnames(d_medians)
-colnames(d_ecdfs)
-group_IDs
-
-# create patient IDs (block IDs) for paired tests (for this data set: 1 block per patient)
+# create block IDs for paired tests (one block per patient, since this is a paired data set)
 patient_IDs <- factor(gsub("_(BCR-XL|Reference)$", "", sample_IDs))
 patient_IDs <- as.numeric(patient_IDs)
 patient_IDs
 
+# check sample IDs, group IDs, and block IDs match correctly
+sample_IDs
+group_IDs
+patient_IDs
+
+
+
+
+################################################
+# Test for differentially abundant (DA) clusters
+################################################
+
 # test for differentially abundant (DA) clusters
-res_DA <- testDA(d_counts, group_IDs, 
-                 paired = TRUE, block_IDs = patient_IDs, 
-                 plot = TRUE, path = "../../../plots")
-
-# show results for top DA clusters
-topTable(res_DA, number = 10)
-
-### results: most micro-clusters are DA (but not all). Try to aggregate them using 
-### hierarchical consensus clustering (then can classify whole groups as DA).
-
-# [TO DO: plot counts for top DA clusters (similar to previous version)]
-
-# [TO DO: plot MST with point color/size for p-value significance]
-
-# [TO DO: plot expression profiles of significant clusters]
-
-# [TO DO: implement cluster/hypothesis merging and aggregation: consensus clustering
-# trees, aggregate p-values]
-
-
-
-##########################################################################################
-# (3b) Test for differential expression of markers within clusters: method "diffcyt-FDA" #
-##########################################################################################
-
-# runtime: ........... with 2 cores on Mac ................. 1 hr 20 mins with a single core
-# runtime: ........ (unpaired tests)
-# (paired tests)
-system.time(
-  res_DE_FDA <- testDE_FDA(d_ecdfs, group_IDs, weighted = TRUE, d_counts = d_counts)
+runtime_DA <- system.time(
+  res_DA <- testDA(d_counts, group_IDs, paired = TRUE, block_IDs = patient_IDs, 
+                   plot = TRUE, path = "../../../plots/diffcyt")
 )
 
-# results for top DE clusters (across all functional markers)
-head(res_DE_FDA, 20)
+# show results
+rowData(res_DA)
+
+# sort to show top (most highly significant) clusters first
+res_DA_sorted <- rowData(res_DA)[order(rowData(res_DA)$adj.P.Val), ]
+
+head(res_DA_sorted, 10)
+#View(res_DA_sorted)
 
 
+# --------
+# analysis
+# --------
 
-# plot ECDFs for top DE cluster
-#plotECDFsTopDECluster(d_ecdfs, path = "../../../plots")
-
-
-# [next: add a heatmap of all the top clusters, to identify them (B cells etc); do the
-# results recover the same biologically meaningful results from the Citrus paper?]
-
+# number of significant DA clusters
+table(res_DA_sorted$adj.P.Val < 0.05)
 
 
-
-
-#####################################################################################
-# (3a) TEST FOR DIFFERENTIAL EXPRESSION OF FUNCTIONAL MARKERS: method "diffcyt-med" #
-#####################################################################################
-
-# re-level factor to use "Reference" as base level
-#group <- factor(group_IDs, levels = c("Reference", "BCR-XL"))
-
-# test for differential expression of functional markers: method "diffcyt-med"
-res_DE_med <- testDE_med(d_clus, group, path = "../../../plots")
-
-# top DE clusters across all functional markers
-# (note: 'coef = 2' selects contrast of interest, i.e. BCR-XL vs. Reference)
-topTable(res_DE_med, coef = 2, number = 20)
+# plots:
+# - counts for top DA cluster
+# - MST with point color/size for p-value
+# - expression profiles of significant clusters, with hierarchical clustering (possibly
+# consensus clustering) to group
 
 
 
 
+#############################################################################
+# Test for differential expression (DE) of functional markers within clusters
+# (method 'diffcyt-med')
+#############################################################################
+
+# test for differential expression (DE) of functional markers within clusters
+runtime_DE_med <- system.time(
+  res_DE_med <- testDE_med(d_counts, d_medians, group_IDs, paired = TRUE, block_IDs = patient_IDs, 
+                           plot = TRUE, path = "../../../plots/diffcyt")
+)
+
+# show results
+rowData(res_DE_med)
+
+# sort to show top (most highly significant) cluster-marker combinations first
+res_DE_med_sorted <- rowData(res_DE_med)[order(rowData(res_DE_med)$adj.P.Val), ]
+
+head(res_DE_med_sorted, 10)
+#View(res_DE_med_sorted)
 
 
+# --------
+# analysis
+# --------
+
+# number of significant DE cluster-marker combinations
+table(res_DE_med_sorted$adj.P.Val < 0.05)
+
+
+# plots:
+# - boxplots (with points) of medians for top cluster-marker combination
+# - one MST plot for each functional marker, with point color = significance, point size =
+# no. of cells
+# - heatmap showing up/down for difference in median marker expression (14 functional
+# markers) for each cluster
+
+
+
+
+#############################################################################
+# Test for differential expression (DE) of functional markers within clusters
+# (method 'diffcyt-FDA')
+#############################################################################
+
+# ----------
+# unweighted
+# ----------
+
+# test for differential expression (DE) of functional markers within clusters
+runtime_DE_FDA_unwtd <- system.time(
+  res_DE_FDA_unwtd <- testDE_FDA(d_counts, d_medians, d_ecdfs, group_IDs, weighted = FALSE, 
+                                 paired = TRUE, block_IDs = patient_IDs, 
+                                 n_perm = 1000, n_cores = 24)
+)
+
+# show results
+rowData(res_DE_FDA_unwtd)
+
+# sort to show top (most highly significant) cluster-marker combinations first
+res_DE_FDA_unwtd_sorted <- rowData(res_DE_FDA_unwtd)[order(rowData(res_DE_FDA_unwtd)$p_adj), ]
+
+head(res_DE_FDA_unwtd_sorted, 10)
+#View(res_DE_FDA_unwtd_sorted)
+
+
+# --------
+# weighted
+# --------
+
+# test for differential expression (DE) of functional markers within clusters
+runtime_DE_FDA_wtd <- system.time(
+  res_DE_FDA_wtd <- testDE_FDA(d_counts, d_medians, d_ecdfs, group_IDs, weighted = TRUE, 
+                               paired = TRUE, block_IDs = patient_IDs, 
+                               n_perm = 1000, n_cores = 24)
+)
+
+# show results
+rowData(res_DE_FDA_wtd)
+
+# sort to show top (most highly significant) cluster-marker combinations first
+res_DE_FDA_wtd_sorted <- rowData(res_DE_FDA_wtd)[order(rowData(res_DE_FDA_wtd)$p_adj), ]
+
+head(res_DE_FDA_wtd_sorted, 10)
+#View(res_DE_FDA_wtd_sorted)
+
+
+# --------
+# analysis
+# --------
+
+# number of significant DE cluster-marker combinations
+
+# unweighted
+table(res_DE_FDA_unwtd_sorted$adj.P.Val < 0.05)
+
+# weighted
+table(res_DE_FDA_wtd_sorted$adj.P.Val < 0.05)
+
+
+# plots (for both unweighted and weighted):
+# - ECDF curves for top cluster-marker combination
+# - one MST plot for each functional marker, with point color = significance, point size =
+# no. of cells
+# - heatmap showing up/down for difference in *median* marker expression (14 functional
+# markers) for each cluster
+
+# additional plots:
+# - heatmap of correlation matrix between unweighted and weighted
+# - runtime for each method (diffcyt-med, diffcyt-FDA-unwtd, diffcyt-FDA-wtd)
 
 
