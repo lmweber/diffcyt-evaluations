9e99edddde88977493fb4f1b9bb2f1da07db48f1
Lukas Weber <lukmweber@gmail.com>
2017-08-31 14:43:24 +0200

    clustering resolution plots: thresholds on same panel

--- AML_sim/3_generate_plots/supp_clustering_resolution/plots_AML_sim_diffcyt_DA_limma_supp_clustering_resolution.R
+++ AML_sim/3_generate_plots/supp_clustering_resolution/plots_AML_sim_diffcyt_DA_limma_supp_clustering_resolution.R
@@ -13,6 +13,7 @@
 library(iCOBRA)
 library(dplyr)
 library(magrittr)
+library(reshape2)
 library(ggplot2)
 library(cowplot)
 
@@ -29,11 +30,12 @@ DIR_PLOTS <- "../../../../plots/AML_sim/supp_clustering_resolution"
 
 
 
-################
-# Generate plots
-################
+################################################################
+# Generate plots: one panel per threshold (th) and condition (j)
+################################################################
 
 # loop over thresholds (th) and conditions (j)
+
 
 # clustering resolution
 resolution <- c(3, 5, 10, 20, 30, 40, 50)
@@ -46,7 +48,7 @@ cond_names <- c("CN", "CBF")
 
 
 # store plots in list
-plots_ROC <- plots_pAUC <- vector("list", length(thresholds) * length(cond_names))
+plots_ROC <- vector("list", length(thresholds) * length(cond_names))
 
 
 for (th in 1:length(thresholds)) {
@@ -97,6 +99,130 @@ for (th in 1:length(thresholds)) {
                                        binary_truth = "spikein", 
                                        aspects = c("roc"))
     
+    # color scheme
+    colors <- colorRampPalette(c("#c7e9c0", "darkgreen"))(7)
+    
+    colors <- colors[1:length(data)]
+    names(colors) <- names(data)
+    
+    # linetypes
+    #linetypes <- 1:length(data)
+    #names(linetypes) <- names(data)
+    
+    # axis ranges
+    x_range <- c(0, 0.5)
+    y_range <- c(0.5, 1)
+    
+    # prepare plotting object
+    cobraplot <- prepare_data_for_plot(cobraperf, colorscheme = colors)
+    
+    # re-order legend
+    cobraplot <- iCOBRA:::reorder_levels(cobraplot, levels = names(data))
+    
+    
+    # ----------
+    # ROC curves
+    # ----------
+    
+    # create plot
+    p <- plot_roc(cobraplot, linewidth = 0.75)
+    
+    # with short title for multi-panel plot
+    p <- p + 
+      #scale_linetype_manual(values = linetypes) + 
+      coord_fixed(xlim = x_range, ylim = y_range) + 
+      xlab("False positive rate") + 
+      ylab("True positive rate") + 
+      ggtitle(paste0(cond_names[j], ", threshold ", gsub("pc$", "\\%", thresholds[th]))) + 
+      theme_bw() + 
+      theme(strip.text.x = element_blank())
+    
+    plots_ROC[[ix]] <- p
+    
+    # save individual panel plot
+    p <- p + 
+      ggtitle(paste0("AML-sim, clustering resolution: diffcyt-DA-limma: ", cond_names[j], ", ", gsub("pc$", "\\%", thresholds[th]), ": ROC curves"))
+    
+    fn <- file.path(DIR_PLOTS, "panels", 
+                    paste0("results_diffcyt_DA_limma_supp_clustering_resolution_ROC_curves_", thresholds[th], "_", cond_names[j], ".pdf"))
+    ggsave(fn, width = 7.5, height = 6)
+    
+  }
+}
+
+
+
+
+#############################################
+# Generate plots: one panel per condition (j)
+#############################################
+
+# loop over conditions (j)
+
+
+# clustering resolution
+resolution <- c(3, 5, 10, 20, 30, 40, 50)
+
+# spike-in thresholds
+thresholds <- c("5pc", "1pc", "0.1pc", "0.01pc")
+
+# condition names
+cond_names <- c("CN", "CBF")
+
+
+# store plots in list
+plots_pAUC <- vector("list", length(cond_names))
+
+
+for (j in 1:length(cond_names)) {
+  
+  # -------------------------------------
+  # Pre-processing steps for iCOBRA plots
+  # -------------------------------------
+  
+  # loop over thresholds (to create one line per threshold)
+  
+  pAUC_list <- vector("list", length(thresholds))
+  names(pAUC_list) <- thresholds
+  
+  for (th in 1:length(thresholds)) {
+    
+    # create 'COBRAData' object
+    data <- list(k_9    = out_diffcyt_DA_limma_supp_resolution[[1]][[th]][[j]], 
+                 k_25   = out_diffcyt_DA_limma_supp_resolution[[2]][[th]][[j]], 
+                 k_100  = out_diffcyt_DA_limma_supp_resolution[[3]][[th]][[j]], 
+                 k_400  = out_diffcyt_DA_limma_supp_resolution[[4]][[th]][[j]], 
+                 k_900  = out_diffcyt_DA_limma_supp_resolution[[5]][[th]][[j]], 
+                 k_1600 = out_diffcyt_DA_limma_supp_resolution[[6]][[th]][[j]], 
+                 k_2500 = out_diffcyt_DA_limma_supp_resolution[[7]][[th]][[j]])
+    
+    # check
+    stopifnot(all(sapply(data, function(d) all(d$spikein == data[[1]]$spikein))))
+    
+    # note: provide all available values
+    # - 'padj' is required for threshold points on TPR-FDR curves
+    # - depending on availability, plotting functions use 'score', then 'pval', then 'padj'
+    cobradata <- COBRAData(pval = data.frame(k_9    = data[["k_9"]][, "p_vals"], 
+                                             k_25   = data[["k_25"]][, "p_vals"], 
+                                             k_100  = data[["k_100"]][, "p_vals"], 
+                                             k_400  = data[["k_400"]][, "p_vals"], 
+                                             k_900  = data[["k_900"]][, "p_vals"], 
+                                             k_1600 = data[["k_1600"]][, "p_vals"], 
+                                             k_2500 = data[["k_2500"]][, "p_vals"]), 
+                           padj = data.frame(k_9    = data[["k_9"]][, "p_adj"], 
+                                             k_25   = data[["k_25"]][, "p_adj"], 
+                                             k_100  = data[["k_100"]][, "p_adj"], 
+                                             k_400  = data[["k_400"]][, "p_adj"], 
+                                             k_900  = data[["k_900"]][, "p_adj"], 
+                                             k_1600 = data[["k_1600"]][, "p_adj"], 
+                                             k_2500 = data[["k_2500"]][, "p_adj"]), 
+                           truth = data.frame(spikein = data[["k_9"]][, "spikein"]))
+    
+    # calculate performance scores
+    # (note: can ignore warning messages when 'padj' not available)
+    cobraperf <- calculate_performance(cobradata, 
+                                       binary_truth = "spikein", 
+                                       aspects = c("roc"))
     
     # calculate pAUC values (store in separate vector)
     pAUC <- rep(NA, length(data))
@@ -127,7 +253,6 @@ for (th in 1:length(thresholds)) {
       pAUC[i] <- roc_i$AUC[roc_i$FPR == thresh]
     }
     
-    
     # calculate AUC values (store in main object) (code from Charlotte)
     # roc(cobraperf) <- 
     #   roc(cobraperf) %>% 
@@ -140,90 +265,53 @@ for (th in 1:length(thresholds)) {
     #   dplyr::ungroup() %>% 
     #   as.data.frame()
     
-    
-    # color scheme
-    colors <- colorRampPalette(c("#c7e9c0", "darkgreen"))(7)
-    
-    colors <- colors[1:length(data)]
-    names(colors) <- names(data)
-    
-    # linetypes
-    #linetypes <- 1:length(data)
-    #names(linetypes) <- names(data)
-    
-    # axis ranges
-    x_range <- c(0, 0.5)
-    y_range <- c(0.5, 1)
-    
-    
-    # prepare plotting object
-    cobraplot <- prepare_data_for_plot(cobraperf, colorscheme = colors)
-    
-    # re-order legend
-    cobraplot <- iCOBRA:::reorder_levels(cobraplot, levels = names(data))
-    
-    
-    
-    # ----------
-    # ROC curves
-    # ----------
-    
-    # create plot
-    p <- plot_roc(cobraplot, linewidth = 0.75)
-    
-    # with short title for multi-panel plot
-    p <- p + 
-      #scale_linetype_manual(values = linetypes) + 
-      coord_fixed(xlim = x_range, ylim = y_range) + 
-      xlab("False positive rate") + 
-      ylab("True positive rate") + 
-      ggtitle(paste0(cond_names[j], ", threshold ", gsub("pc$", "\\%", thresholds[th]))) + 
-      theme_bw() + 
-      theme(strip.text.x = element_blank())
-    
-    plots_ROC[[ix]] <- p
-    
-    # save individual panel plot
-    p <- p + 
-      ggtitle(paste0("AML-sim, clustering resolution: diffcyt-DA-limma: ", cond_names[j], ", ", gsub("pc$", "\\%", thresholds[th]), ": ROC curves"))
-    
-    fn <- file.path(DIR_PLOTS, "panels", 
-                    paste0("results_diffcyt_DA_limma_supp_clustering_resolution_ROC_curves_", thresholds[th], "_", cond_names[j], ".pdf"))
-    ggsave(fn, width = 7.5, height = 6)
-    
-    
-    
-    # ----------
-    # pAUC plots
-    # ----------
-    
-    p_data <- data.frame(method = factor(names(pAUC), levels = names(pAUC)), pAUC = pAUC)
-    
-    y_range_pAUC <- c(0, thresh)
-    
-    # create plot
-    p <- 
-      ggplot(p_data, aes(x = method, y = pAUC, group = 1)) + 
-      geom_line(color = "dodgerblue4", lwd = 0.7) + 
-      geom_point(color = "dodgerblue4", size = 2.25) + 
-      xlab("Number of clusters") + 
-      ylim(y_range_pAUC) + 
-      ylab(paste0("pAUC (FPR < ", thresh, ")")) + 
-      theme_bw() + 
-      ggtitle(paste0(cond_names[j], ", threshold ", gsub("pc$", "\\%", thresholds[th]))) + 
-      theme(legend.position = "none")
-    
-    plots_pAUC[[ix]] <- p
-    
-    # save individual panel plot
-    p <- p + 
-      ggtitle(paste0("AML-sim, clustering resolution: diffcyt-DA-limma: ", cond_names[j], ", ", gsub("pc$", "\\%", thresholds[th]), ": pAUC (FPR < ", thresh, ")"))
-    
-    fn <- file.path(DIR_PLOTS, "panels", 
-                    paste0("results_diffcyt_DA_limma_supp_clustering_resolution_pAUC_", thresholds[th], "_", cond_names[j], ".pdf"))
-    ggsave(fn, width = 7.5, height = 6)
-    
+    # store pAUC values
+    pAUC_list[[th]] <- pAUC
   }
+  
+  # create data frame for plotting
+  for (z in 1:length(pAUC_list)) stopifnot(all(names(pAUC_list[[z]]) == names(pAUC_list[[1]])))
+  
+  p_data <- as.data.frame(do.call("cbind", pAUC_list))
+  p_data$k_clusters <- factor(names(pAUC_list[[1]]), levels = names(pAUC_list[[1]]))
+  p_data <- melt(p_data, id.vars = "k_clusters", variable.name = "threshold", value.name = "pAUC")
+  
+  # color scheme
+  colors <- colorRampPalette(c("#023858", "#d0d1e6"))(4)
+  
+  colors <- colors[1:length(pAUC_list)]
+  names(colors) <- names(pAUC_list)
+  
+  # axis ranges
+  y_range_pAUC <- c(0, thresh)
+  
+  
+  # ----------
+  # pAUC plots
+  # ----------
+  
+  # create plot
+  p <- 
+    ggplot(p_data, aes(x = k_clusters, y = pAUC, group = threshold, color = threshold)) + 
+    geom_line(lwd = 0.7) + 
+    geom_point(size = 2.25) + 
+    scale_color_manual(values = colors) + 
+    xlab("Number of clusters") + 
+    ylim(y_range_pAUC) + 
+    ylab(paste0("pAUC (FPR < ", thresh, ")")) + 
+    theme_bw() + 
+    ggtitle(cond_names[j])
+  
+  plots_pAUC[[j]] <- p
+  
+  # save individual panel plot
+  p <- p + 
+    ggtitle(paste0("AML-sim, clustering resolution: diffcyt-DA-limma: ", cond_names[j], ": pAUC (FPR < ", thresh, ")"))
+  
+  fn <- file.path(DIR_PLOTS, "panels", 
+                  paste0("results_diffcyt_DA_limma_supp_clustering_resolution_pAUC_", cond_names[j], ".pdf"))
+  ggsave(fn, width = 7.5, height = 6.5)
+  
 }
 
 
@@ -281,27 +369,27 @@ plots_pAUC <- lapply(plots_pAUC, function(p) {
 })
 
 # format into grid
-grid_pAUC <- do.call(plot_grid, append(plots_pAUC, list(labels = "AUTO", nrow = 4, ncol = 2, 
+grid_pAUC <- do.call(plot_grid, append(plots_pAUC, list(labels = "AUTO", nrow = 1, ncol = 2, 
                                                         scale = 0.95, label_y = 0.975)))
 
 # add combined axis titles
 xaxis_pAUC <- ggdraw() + draw_label("Number of clusters", size = 12)
 yaxis_pAUC <- ggdraw() + draw_label(paste0("pAUC (FPR < ", thresh, ")"), size = 12, angle = 90)
 
-grid_pAUC <- plot_grid(grid_pAUC, xaxis_pAUC, ncol = 1, rel_heights = c(50, 1))
+grid_pAUC <- plot_grid(grid_pAUC, xaxis_pAUC, ncol = 1, rel_heights = c(15, 1))
 grid_pAUC <- plot_grid(yaxis_pAUC, grid_pAUC, nrow = 1, rel_widths = c(1, 30))
 
 # add combined legend
-#legend_pAUC <- get_legend(plots_pAUC[[1]] + theme(legend.position = "right"))
-#grid_pAUC <- plot_grid(grid_pAUC, legend_pAUC, nrow = 1, rel_widths = c(5, 1))
+legend_pAUC <- get_legend(plots_pAUC[[1]] + theme(legend.position = "right"))
+grid_pAUC <- plot_grid(grid_pAUC, legend_pAUC, nrow = 1, rel_widths = c(10, 1))
 
 # add combined title
 title_pAUC <- ggdraw() + draw_label(paste0("AML-sim, clustering resolution: diffcyt-DA-limma: pAUC (FPR < ", thresh, ")"), fontface = "bold")
-grid_pAUC <- plot_grid(title_pAUC, grid_pAUC, ncol = 1, rel_heights = c(1, 32))
+grid_pAUC <- plot_grid(title_pAUC, grid_pAUC, ncol = 1, rel_heights = c(1, 12))
 
 # save plots
 fn_pAUC <- file.path(DIR_PLOTS, "results_diffcyt_DA_limma_supp_clustering_resolution_pAUC.pdf")
-ggsave(fn_pAUC, grid_pAUC, width = 10, height = 14.14)
+ggsave(fn_pAUC, grid_pAUC, width = 10, height = 4)
 
 
 
