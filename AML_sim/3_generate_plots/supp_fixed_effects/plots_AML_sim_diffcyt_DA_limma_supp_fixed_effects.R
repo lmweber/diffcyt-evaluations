9a3a1e30d9930d22090f603654b06c56ad131aee
Lukas Weber <lukmweber@gmail.com>
2017-09-18 21:16:27 +0200

    colours

--- AML_sim/3_generate_plots/supp_fixed_effects/plots_AML_sim_diffcyt_DA_limma_supp_fixed_effects.R
+++ AML_sim/3_generate_plots/supp_fixed_effects/plots_AML_sim_diffcyt_DA_limma_supp_fixed_effects.R
@@ -81,7 +81,7 @@ for (th in 1:length(thresholds)) {
     
     # color scheme
     #colors <- c("darkslategray1", "darkslategray1")
-    colors <- c("black", "black")
+    colors <- c("darkgray", "darkgray")
     
     colors <- colors[1:length(data)]
     names(colors) <- names(data)
