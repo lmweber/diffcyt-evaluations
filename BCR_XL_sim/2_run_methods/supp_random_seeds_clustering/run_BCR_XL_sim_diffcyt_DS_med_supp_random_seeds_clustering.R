b529dee525345807ec2a8c1b6b41f06ead87562a
Lukas Weber <lukmweber@gmail.com>
2017-11-23 18:19:29 +0100

    varying random seeds for clustering step

---
+++ BCR_XL_sim/2_run_methods/supp_random_seeds_clustering/run_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.R
@@ -0,0 +1,297 @@
+##########################################################################################
+# Script to run methods
+# 
+# - method: diffcyt-DS-med
+# - data set: BCR-XL-sim
+# 
+# - supplementary results: varying random seeds for clustering
+# 
+# Lukas Weber, November 2017
+##########################################################################################
+
+
+library(diffcyt)
+library(flowCore)
+library(SummarizedExperiment)
+
+
+DIR_BENCHMARK <- "../../../../../benchmark_data/BCR_XL_sim/data/main"
+DIR_PLOTS <- "../../../../plots/BCR_XL_sim/supp_random_seeds_clustering/diagnostic/diffcyt_DS_med"
+DIR_RDATA <- "../../../../RData/BCR_XL_sim/supp_random_seeds_clustering"
+DIR_SESSION_INFO <- "../../../../session_info/BCR_XL_sim/supp_random_seeds_clustering"
+
+
+
+
+#############
+# Preliminary
+#############
+
+# contrast (to compare 'spike' vs. 'base')
+# note: include zeros for patient_IDs fixed effects
+contrasts_list <- list(spike = c(0, 1, 0, 0, 0, 0, 0, 0, 0))
+
+# varying random seeds for clustering
+seeds <- c(1001, 1002, 1003)
+
+# lists to store objects
+out_diffcyt_DS_med_supp_random_seeds_clustering <- 
+  out_clusters_diffcyt_DS_med_supp_random_seeds_clustering <- 
+  out_objects_diffcyt_DS_med_supp_random_seeds_clustering <- 
+  runtime_diffcyt_DS_med_supp_random_seeds_clustering <- vector("list", length(seeds))
+names(out_diffcyt_DS_med_supp_random_seeds_clustering) <- 
+  names(out_clusters_diffcyt_DS_med_supp_random_seeds_clustering) <- 
+  names(out_objects_diffcyt_DS_med_supp_random_seeds_clustering) <- 
+  names(runtime_diffcyt_DS_med_supp_random_seeds_clustering) <- seeds
+
+
+
+
+for (s in 1:length(seeds)) {
+  
+  
+  ###########################
+  # Load data, pre-processing
+  ###########################
+  
+  # filenames
+  files <- list.files(DIR_BENCHMARK, pattern = "\\.fcs$", full.names = TRUE)
+  files_base <- files[grep("base\\.fcs$", files)]
+  files_spike <- files[grep("spike\\.fcs$", files)]
+  
+  # load data
+  files_load <- c(files_base, files_spike)
+  files_load
+  
+  d_input <- lapply(files_load, read.FCS, transformation = FALSE, truncate_max_range = FALSE)
+  
+  # sample IDs, group IDs, patient IDs
+  sample_IDs <- gsub("^BCR_XL_sim_", "", 
+                     gsub("\\.fcs$", "", basename(files_load)))
+  sample_IDs
+  
+  group_IDs <- factor(gsub("^.*_", "", sample_IDs), levels = c("base", "spike"))
+  group_IDs
+  
+  patient_IDs <- factor(gsub("_.*$", "", sample_IDs))
+  patient_IDs
+  
+  # check
+  data.frame(sample_IDs, group_IDs, patient_IDs)
+  
+  # indices of all marker columns, lineage markers, and functional markers
+  # (10 surface markers / 14 functional markers; see Bruggner et al. 2014, Table 1)
+  cols_markers <- c(3:4, 7:9, 11:19, 21:22, 24:26, 28:31, 33)
+  cols_lineage <- c(3:4, 9, 11, 12, 14, 21, 29, 31, 33)
+  cols_func <- setdiff(cols_markers, cols_lineage)
+  
+  
+  
+  
+  ##################
+  # diffcyt pipeline
+  ##################
+  
+  # --------------------
+  # pre-processing steps
+  # --------------------
+  
+  runtime_preprocessing <- system.time({
+    
+    # prepare data into required format
+    d_se <- prepareData(d_input, sample_IDs, group_IDs, 
+                        cols_markers, cols_lineage, cols_func)
+    
+    colnames(d_se)[cols_lineage]
+    colnames(d_se)[cols_func]
+    
+    # transform data
+    d_se <- transformData(d_se, cofactor = 5)
+    
+    # clustering
+    # (runtime: ~5 sec with xdim = 10, ydim = 10)
+    seed <- seeds[s]
+    d_se <- generateClusters(d_se, xdim = 10, ydim = 10, seed = seed)
+    
+    length(table(rowData(d_se)$cluster))  # number of clusters
+    nrow(rowData(d_se))                   # number of cells
+    sum(table(rowData(d_se)$cluster))
+    min(table(rowData(d_se)$cluster))     # size of smallest cluster
+    max(table(rowData(d_se)$cluster))     # size of largest cluster
+    
+    # calculate cluster cell counts
+    d_counts <- calcCounts(d_se)
+    
+    dim(d_counts)
+    rowData(d_counts)
+    length(assays(d_counts))
+    
+    # calculate cluster medians by sample
+    d_medians <- calcMedians(d_se)
+    
+    dim(d_medians)
+    rowData(d_medians)
+    length(assays(d_medians))
+    names(assays(d_medians))
+    
+    # calculate cluster medians across all samples
+    d_medians_all <- calcMediansAll(d_se)
+    
+    dim(d_medians_all)
+    length(assays(d_medians_all))
+    
+  })
+  
+  
+  # ---------------------------------
+  # store data objects (for plotting)
+  # ---------------------------------
+  
+  out_objects_diffcyt_DS_med_supp_random_seeds_clustering[[s]] <- 
+    list(d_se = d_se, 
+         d_counts = d_counts, 
+         d_medians = d_medians, 
+         d_medians_all = d_medians_all)
+  
+  
+  # -------------------------------------------------------
+  # test for differential functional states within clusters
+  # -------------------------------------------------------
+  
+  runtime_tests <- system.time({
+    
+    # set up design matrix
+    # note: include 'patient_IDs' as fixed effects ('block_IDs' argument)
+    design <- createDesignMatrix(group_IDs, block_IDs = patient_IDs)
+    design
+    
+    # set up contrast matrix
+    contrast <- createContrast(group_IDs, contrast = contrasts_list$spike)
+    contrast
+    
+    # run tests
+    # note: including 'patient_IDs' as fixed effects in design matrix
+    res <- testDS_med(d_counts, d_medians, design, contrast, path = DIR_PLOTS)
+    
+  })
+  
+  # show results
+  rowData(res)
+  
+  # sort to show top (most highly significant) cluster-marker combinations first
+  res_sorted <- rowData(res)[order(rowData(res)$adj.P.Val), ]
+  print(head(res_sorted, 10))
+  #View(as.data.frame(res_sorted))
+  
+  # number of significant tests (note: one test per cluster-marker combination)
+  print(table(res_sorted$adj.P.Val <= 0.1))
+  
+  # runtime (~30 sec on laptop)
+  runtime_total <- runtime_preprocessing[["elapsed"]] + runtime_tests[["elapsed"]]
+  print(runtime_total)
+  
+  runtime_diffcyt_DS_med_supp_random_seeds_clustering[[s]] <- runtime_total
+  
+  
+  # ---------------------------------------------
+  # store results at cluster level (for plotting)
+  # ---------------------------------------------
+  
+  res_clusters <- as.data.frame(rowData(res))
+  
+  out_clusters_diffcyt_DS_med_supp_random_seeds_clustering[[s]] <- res_clusters
+  
+  
+  
+  
+  
+  ##############################
+  # Return results at cell level
+  ##############################
+  
+  # Note: diffcyt methods return results for each cluster-marker combination. To enable
+  # performance comparisons between methods at the cell level, we assign the same p-values
+  # to all cells within a given cluster-marker combination.
+  
+  # Note: return cell-level results for marker pS6 only, since the comparative evaluations
+  # (ROC curves etc) are based on pS6 in B cells only.
+  
+  
+  # number of cells per sample (including spike-in cells)
+  n_cells <- sapply(d_input, nrow)
+  
+  # identify B cells (these contain the true differential signal; from both 'base' and
+  # 'spike' conditions)
+  is_B_cell <- unlist(sapply(d_input, function(d) exprs(d)[, "B_cell"]))
+  stopifnot(length(is_B_cell) == sum(n_cells))
+  
+  
+  # match cluster-level p-values for marker pS6 to individual cells
+  
+  stopifnot(nrow(rowData(res)) == nlevels(rowData(d_se)$cluster) * length(cols_func), 
+            all(levels(rowData(res)$cluster) == levels(rowData(d_se)$cluster)), 
+            all(levels(rowData(res)$cluster) %in% rowData(res)$cluster))
+  
+  # select results for pS6
+  res_pS6 <- res[rowData(res)$marker == "pS6(Yb172)Dd", ]
+  
+  # match cells to clusters
+  ix_match <- match(rowData(d_se)$cluster, rowData(res_pS6)$cluster)
+  
+  p_vals_clusters <- rowData(res_pS6)$P.Value
+  p_adj_clusters <- rowData(res_pS6)$adj.P.Val
+  
+  p_vals_cells <- p_vals_clusters[ix_match]
+  p_adj_cells <- p_adj_clusters[ix_match]
+  
+  
+  # set up data frame with results (for marker pS6) and true B-cell status at cell level
+  
+  res_p_vals <- p_vals_cells
+  res_p_adj <- p_adj_cells
+  
+  # replace NAs (due to filtering) to ensure same cells are returned for all methods
+  res_p_vals[is.na(res_p_vals)] <- 1
+  res_p_adj[is.na(res_p_adj)] <- 1
+  
+  stopifnot(length(res_p_vals) == length(res_p_adj), 
+            length(res_p_vals) == length(is_B_cell))
+  
+  res <- data.frame(p_vals = res_p_vals, 
+                    p_adj = res_p_adj, 
+                    B_cell = is_B_cell)
+  
+  # store results
+  out_diffcyt_DS_med_supp_random_seeds_clustering[[s]] <- res
+
+}
+
+
+
+
+#####################
+# Save output objects
+#####################
+
+save(out_diffcyt_DS_med_supp_random_seeds_clustering, runtime_diffcyt_DS_med_supp_random_seeds_clustering, 
+     file = file.path(DIR_RDATA, "outputs_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.RData"))
+
+save(out_clusters_diffcyt_DS_med_supp_random_seeds_clustering, 
+     file = file.path(DIR_RDATA, "out_clusters_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.RData"))
+
+save(out_objects_diffcyt_DS_med_supp_random_seeds_clustering, 
+     file = file.path(DIR_RDATA, "out_objects_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.RData"))
+
+
+
+
+#####################
+# Session information
+#####################
+
+sink(file.path(DIR_SESSION_INFO, "session_info_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.txt"))
+sessionInfo()
+sink()
+
+
+
---
+++ BCR_XL_sim/3_generate_plots/supp_random_seeds_clustering/plots_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.R
@@ -0,0 +1,123 @@
+##########################################################################################
+# Generate plots
+# 
+# - data set: BCR-XL-sim
+# - plot type: performance metrics
+# - method: diffcyt-DS-med
+# 
+# - supplementary results: varying random seeds for clustering
+# 
+# Lukas Weber, November 2017
+##########################################################################################
+
+
+library(iCOBRA)
+library(ggplot2)
+library(cowplot)  # note: cowplot masks 'ggsave' from ggplot2
+
+
+# load saved results
+DIR_RDATA_SUPP_MAIN <- "../../../../RData/BCR_XL_sim/main"
+DIR_RDATA_SUPP_RANDOM_SEEDS_CLUSTERING <- "../../../../RData/BCR_XL_sim/supp_random_seeds_clustering"
+
+load(file.path(DIR_RDATA_SUPP_MAIN, "outputs_BCR_XL_sim_diffcyt_DS_med_main.RData"))
+load(file.path(DIR_RDATA_SUPP_RANDOM_SEEDS_CLUSTERING, "outputs_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering.RData"))
+
+
+# path to save plots
+DIR_PLOTS <- "../../../../plots/BCR_XL_sim/supp_random_seeds_clustering"
+
+
+
+
+################
+# Generate plots
+################
+
+# -------------------------------------
+# Pre-processing steps for iCOBRA plots
+# -------------------------------------
+
+# create 'COBRAData' object
+data <- list(seed_main = out_diffcyt_DS_med_main, 
+             seed_alt_1 = out_diffcyt_DS_med_supp_random_seeds_clustering[[1]], 
+             seed_alt_2 = out_diffcyt_DS_med_supp_random_seeds_clustering[[2]], 
+             seed_alt_3 = out_diffcyt_DS_med_supp_random_seeds_clustering[[3]])
+
+# check
+stopifnot(all(sapply(data, function(d) all(d$B_cell == data[[1]]$B_cell))))
+
+# note: provide all available values
+# 'padj' is required for threshold points on TPR-FDR curves
+# depending on availability, plotting functions use 'score', then 'pval', then 'padj'
+cobradata <- COBRAData(pval = data.frame(seed_main = data[["seed_main"]][, "p_vals"], 
+                                         seed_alt_1 = data[["seed_alt_1"]][, "p_vals"], 
+                                         seed_alt_2 = data[["seed_alt_2"]][, "p_vals"], 
+                                         seed_alt_3 = data[["seed_alt_3"]][, "p_vals"]), 
+                       padj = data.frame(seed_main = data[["seed_main"]][, "p_adj"], 
+                                         seed_alt_1 = data[["seed_alt_1"]][, "p_adj"], 
+                                         seed_alt_2 = data[["seed_alt_2"]][, "p_adj"], 
+                                         seed_alt_3 = data[["seed_alt_3"]][, "p_adj"]), 
+                       truth = data.frame(B_cell = data[["seed_main"]][, "B_cell"]))
+
+# calculate performance scores
+# (note: can ignore warning messages when 'padj' not available)
+cobraperf <- calculate_performance(cobradata, 
+                                   binary_truth = "B_cell", 
+                                   aspects = "roc")
+
+# color scheme
+colors <- c("darkblue", "darkblue", "darkblue", "darkblue")
+
+colors <- colors[1:length(data)]
+names(colors) <- names(data)
+
+# linetypes
+linetypes <- c("solid", "dashed", "dashed", "dashed")
+names(linetypes) <- names(data)
+
+# prepare plotting object
+cobraplot <- prepare_data_for_plot(cobraperf, 
+                                   colorscheme = colors, 
+                                   conditionalfill = FALSE)
+
+# re-order legend
+cobraplot <- reorder_levels(cobraplot, levels = names(data))
+
+
+
+# ----------
+# ROC curves
+# ----------
+
+# create plot
+p_ROC <- 
+  plot_roc(cobraplot, linewidth = 0.75) + 
+  scale_linetype_manual(values = linetypes, guide = FALSE) + 
+  coord_fixed() + 
+  xlab("False positive rate") + 
+  ylab("True positive rate") + 
+  ggtitle("BCR-XL-sim: random seeds for clustering", subtitle = "ROC curves") + 
+  theme_bw() + 
+  theme(strip.text.x = element_blank()) + 
+  guides(color = guide_legend("random seed"), linetype = guide_legend("random seed"))
+
+# save plot
+fn <- file.path(DIR_PLOTS, "results_BCR_XL_sim_diffcyt_DS_med_supp_random_seeds_clustering_ROC.pdf")
+ggsave(fn, width = 4.75, height = 3.5)
+
+
+
+
+###################################
+# Save timestamp file for Makefiles
+###################################
+
+file_timestamp <- file.path(DIR_PLOTS, "timestamp.txt")
+
+sink(file_timestamp)
+Sys.time()
+sink()
+
+
+
