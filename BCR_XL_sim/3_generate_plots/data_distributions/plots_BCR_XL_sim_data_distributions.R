59f172526c7a716814166e4b91d4b5ea926faa9e
Lukas Weber <lukmweber@gmail.com>
2018-05-06 19:07:03 +0200

    plot formatting

--- BCR_XL_sim/3_generate_plots/data_distributions/plots_BCR_XL_sim_data_distributions.R
+++ BCR_XL_sim/3_generate_plots/data_distributions/plots_BCR_XL_sim_data_distributions.R
@@ -267,7 +267,7 @@ d_plot <- d_plot[d_plot$B_cell == 1, -which(colnames(d_plot) == "B_cell")]
 d_plot <- melt(d_plot, id.vars = "group", variable.name = "marker", value.name = "expression")
 
 # marker types
-d_plot$marker_class <- factor(as.numeric(d_plot$marker %in% names_state), labels = c("cell type", "state"))
+d_plot$marker_class <- factor(as.numeric(d_plot$marker %in% names_state), labels = c("cell type", "cell state"))
 
 d_plot$marker <- factor(d_plot$marker, levels = rev(levels(d_plot$marker)))
 
@@ -335,7 +335,7 @@ table(d_plot$group, d_plot$less_distinct)
 d_plot <- melt(d_plot, id.vars = c("group", "less_distinct"), variable.name = "marker", value.name = "expression")
 
 # marker types
-d_plot$marker_class <- factor(as.numeric(d_plot$marker %in% names_state), labels = c("cell type", "state"))
+d_plot$marker_class <- factor(as.numeric(d_plot$marker %in% names_state), labels = c("cell type", "cell state"))
 
 d_plot$marker <- factor(d_plot$marker, levels = rev(levels(d_plot$marker)))
 
